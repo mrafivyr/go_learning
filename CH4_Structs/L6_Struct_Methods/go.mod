0a4afab0a1b8181d09d49bbee4b51ab191adf6d2
mrafivyr <rafi338@gmail.com>
2025-08-30 21:00:22 +0530

    Chapter 4: L6 Struct Methods

---
+++ CH4_Structs/L6_Struct_Methods/go.mod
@@ -0,0 +1,3 @@
+module l6_struct_methods
+
+go 1.23.4
---
+++ CH4_Structs/L6_Struct_Methods/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+type authenticationInfo struct {
+	username string
+	password string
+}
+
+// create the method below
+func (a authenticationInfo) getBasicAuth() string {
+	return fmt.Sprintf("Authorization: Basic %s:%s", a.username, a.password)
+}
